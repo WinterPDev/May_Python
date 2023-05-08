26d3d762dada1ccc2df3ed6c43c0bd8fd42533dc
WinterP <winterp.dev@gmail.com>
2023-05-08 13:28:38 -0500

    W2D1 Update

---
+++ Week_2/MySQL/mySQL_Notes.py
@@ -0,0 +1,16 @@
+'''
+
+
+ERD ( Entity Relationship Diagram )
+
+Data Table Maps / Visualizations
+
+ID (Primary Key) : The identifier for the piece of information that is unique to the data table.
+- Remember to enable the 'AI' column for your ID so it will Auto-Increment!
+
+- ALWAYS INCLUDE A created_at and updated_at value:
+---> Set the defaults for created_at as 'NOW()'
+---> Set the defaults for updated_at as 'NOW() ON UPDATE NOW()'
+
+
+'''
